1b028c8ba3c5f4cdc2d472a3de27ce47c961e5e8
Jean-Claude Bissou <jeanclaudebissou@gmail.com>
2022-10-01 14:27:00 -0500

    update authorization to allowed headers

--- netlify_functions/cors.js
+++ netlify_functions/cors.js
@@ -42,7 +42,7 @@ exports.handler = async (event, context) => {
 			"content-type": String(headers["content-type"]) || "text/plain",
 			"access-control-allow-origin": "http://127.0.0.1:5501",
 			"access-control-allow-methods": "GET, HEAD, OPTIONS, POST, PUT",
-			"access-control-allow-headers": "access-control-allow-headers, origin, accept, x-requested-with, content-type, access-control-request-method, access-control-request-headers"
+			"access-control-allow-headers": "access-control-allow-headers, origin, accept, x-requested-with, content-type, access-control-request-method, access-control-request-headers, authorization"
 		},
 		multiValueHeaders: {
 			"set-cookie": cookie_header || []
